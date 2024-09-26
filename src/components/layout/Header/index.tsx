a25caeb3e013d83d56266302f13a5087bafd6a07
Gustavo-dev <playerforever81@gmail.com>
2024-09-25 22:02:50 -0300

    fix: start of header

--- src/components/layout/Header/index.tsx
+++ src/components/layout/Header/index.tsx
@@ -1,23 +1,28 @@
 import React from "react";
 import styles from "@/components/layout/Header/style.module.css";
+import DownArrow from "@/components/shared/DownArrow";
+import ShoppingCart from "@/components/shared/ShoppingCart";
 
 const Header = () => {
   return (
     <div className={styles.container}>
-      <div className={styles.navBar}>
-        <div className={styles.navBarLeft}>
-          <h1>codepay.</h1>
+      <div className={styles.navBarLeft}>
+        <h1>codepay.</h1>
 
-          <h2>Início</h2>
-          <h2>Comprar</h2>
-          <h2>Contato</h2>
-          <h2>Ajuda</h2>
+        <p>Início</p>
+        <p>Comprar</p>
+        <p>Contato</p>
+        <p>Ajuda</p>
+      </div>
+
+      <div className={styles.navBarRight}>
+        <div className={styles.account}>
+          Conta <DownArrow />
         </div>
 
-        <div className={styles.navBarRight}>
-          <div className={styles.account}>Conta</div>
-
-          <div className={styles.shoppingCart}> 2 items</div>
+        <div className={styles.shoppingCart}>
+          {" "}
+          2 items <ShoppingCart />
         </div>
       </div>
     </div>
