5b0614e281360d847115309f6719625f5cba866d
gustavo-gthb <playerforever81@gmail.com>
2024-10-26 21:02:37 -0300

    fix: Imported the account, shipping and payment icons.

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,14 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import Header from "./../components/layout/Header/index";
+import Main from '../components/layout/Main/index';
 
 export default function Home() {
   return (
     <>
       <Header />
       <main className={styles.main}>
-
+        <Main/>
       </main>
       
     </>
--- src/components/layout/Main/index.tsx
+++ src/components/layout/Main/index.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import styles from "@/components/layout/Main/style.module.css";
+import Account from '../../shared/Account/index';
+import Shipping from '../../shared/Shipping/index';
+import Payment from '../../shared/Payment/index';
 
 const Main = () => {
   return (
     <div className={styles.container}>
       <div className={styles.purchaseProgress}>
-        <div className={styles.account}></div>
-        <div className={styles.shipping}></div>
-        <div className={styles.payment}></div>
+        <div className={styles.account}><Account/> Conta</div>
+        <div className={styles.shipping}><Shipping/> Envio</div>
+        <div className={styles.payment}><Payment/> Pagamento</div>
       </div>
     </div>
   );
