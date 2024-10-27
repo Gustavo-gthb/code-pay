233a10e90b768347af804e05b8645f11f59b7532
gustavo-gthb <playerforever81@gmail.com>
2024-10-26 23:49:32 -0300

    fix: added registration form

--- src/components/layout/AccounDetails/Register/index.tsx
+++ src/components/layout/AccounDetails/Register/index.tsx
@@ -1,10 +1,34 @@
-import React from 'react'
-import styles from "@/components/layout/AccounDetails/Register/style.module.css"
+import React from "react";
+import styles from "@/components/layout/AccounDetails/Register/style.module.css";
 
 const Register = () => {
   return (
-    <div className={styles.container}>Register</div>
-  )
-}
+    <div className={styles.container}>
+      <h2>Detalhes do envio</h2>
 
-export default Register
+      <form action="">
+        <p>Usar o endereço salvo</p>
+        <input type="address" />
+
+        <p>Cidade</p>
+        <input type="text" />
+
+        <p>Rua, bairro</p>
+        <input type="text" />
+
+        <div className={styles.numberState}>
+            <p>Número</p>
+            <input type="number" />
+
+            <p>Estado</p>
+        </div>
+      </form>
+
+        <button>Registra-se para conta</button>
+        <button>Entrar</button>
+
+    </div>
+  );
+};
+
+export default Register;
